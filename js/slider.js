181514ba5a3dd7679f8888613064556b71b76f32
gon_14 <poniemangon@gmail.com>
2024-02-09 13:24:22 -0300

    banner2

--- js/slider.js
+++ js/slider.js
@@ -30,4 +30,12 @@ $('.carousel').slick({
       }
     ]
   });
+
+  $('.carousel2').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
       
